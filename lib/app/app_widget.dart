03f74e7999fb433a904022ed61ace3e24eb74728
Eder Costa <eder@b2mlportal.com.br>
2022-06-08 16:51:08 -0300

    config azuca

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:asuka/asuka.dart' as asuka;
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -10,6 +11,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
         title: 'Job Timer',
+        builder: asuka.builder,
         routeInformationParser: Modular.routeInformationParser,
         routerDelegate: Modular.routerDelegate);
   }
