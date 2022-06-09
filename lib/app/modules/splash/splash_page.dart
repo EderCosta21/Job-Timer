de183250674bd15a58523384ab6b4949e8a28785
Eder Costa <eder@b2mlportal.com.br>
2022-06-08 22:47:44 -0300

    splash login

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:job_timer/app/core/ui/job_timer_icons.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -8,8 +9,22 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Splash')),
-      body: Container(),
+      body: Container(
+        // ignore: prefer_const_constructors
+        decoration: BoxDecoration(
+          // ignore: prefer_const_constructors
+          gradient: LinearGradient(
+            // ignore: prefer_const_literals_to_create_immutables
+            colors: [
+              const Color(0XFF009289),
+              const Color(0XFF0167B2),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Image.asset('assets/images/logo.png'),
+        ),
+      ),
     );
   }
 }
