80a645d9cca17c2b61d1907d0c9dba2259093ae9
Eder Costa <eder@b2mlportal.com.br>
2022-06-09 14:57:34 -0300

    Conexão com o banco de dados

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/core/database/database_impl.dart';
 import 'package:job_timer/app/modules/splash/splash_page.dart';
 import 'package:job_timer/app/services/auth/auth_service_impl.dart';
 
@@ -9,6 +10,7 @@ class AppModule extends Module {
   @override
   List<Bind> get binds => [
         Bind.lazySingleton((i) => AuthServiceImpl()),
+        Bind.lazySingleton((i) => DatabaseImpl()),
       ];
 
   @override
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/entities/project.dart';
+import 'package:job_timer/app/entities/project_status.dart';
+import '../../core/database/database.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,10 +10,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                final db = Modular.get<Database>();
+
+                final connection = await db.openConnection();
+
+                connection.writeTxn((isar) {
+                  var project = Project();
+
+                  project.name = 'Projeto Teste';
+                  project.status = ProjectStatus.finalizado;
+
+                  return connection.projects.put(project);
+                });
+              },
+              child: Text('Cadastrar'),
+            ),
+          ],
+        ));
   }
 }
